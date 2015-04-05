89034d85e939c71709e4ce6ac70a9203bfa5f262
Vincent Ollivier <v@vinc.cc>
2015-04-06 08:57:15 +1200

    Fix timezone in Python script

--- ephemeris.py
+++ ephemeris.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import ephem
+from datetime import timezone
 
 ephemeris = {
     'solstices': [],
@@ -10,12 +11,14 @@ ephemeris = {
 date = ephem.date('1970-01-01') # Unix Epoch
 while date < ephem.date('2033-05-18'): # 2 gigaseconds later
     date = ephem.next_solstice(date)
-    ephemeris['solstices'].append(int(date.datetime().timestamp() * 1000))
+    time = int(date.datetime().replace(tzinfo=timezone.utc).timestamp() * 1000)
+    ephemeris['solstices'].append(time)
 
 date = ephem.date('1970-01-01') # Unix Epoch
 while date < ephem.date('2033-05-18'): # 2 gigaseconds later
     date = ephem.next_new_moon(date)
-    ephemeris['new_moons'].append(int(date.datetime().timestamp() * 1000))
+    time = int(date.datetime().replace(tzinfo=timezone.utc).timestamp() * 1000)
+    ephemeris['new_moons'].append(time)
 
 with open('ephemeris.json', 'w') as f:
-    json.dump(ephemeris, f)
+    json.dump(ephemeris, f, indent=2)
